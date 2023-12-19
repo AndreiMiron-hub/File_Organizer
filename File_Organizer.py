1902c34eac75fd88562dd7e8c07eb23e0672ab2f
Andrei Miron <andreimiron001@gmail.com>
2023-12-19 13:34:18 +0200

    Remove unnecessary imports, clean existing code

--- File_Organizer.py
+++ File_Organizer.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
  
  
@@ -31,28 +30,10 @@ FILE_FORMATS = {file_format: directory
 
 downloads_path = Path.home() / 'Downloads'
 
-
-for file in os.scandir(downloads_path):
-    file_path = Path(file)
+for file_path in Path.iterdir(downloads_path):
     file_format = file_path.suffix.lower()
+    file = file_path.stem + file_format
     if file_format in FILE_FORMATS:
-        new_file_path = Path(file_path)
-        downloads_path.mkdir(exist_ok=True)
-        os.rename(file_path, file_path)
- 
-# downloaded_images_path = downloads_path + '\\Images'
-# downloaded_pdf_path = downloads_path + '\\Docs'
-# downloaded_exe_path = downloads_path + '\\Executables'
-
-# Path(downloaded_images_path).mkdir(parents=True, exist_ok=True)
-# Path(downloaded_pdf_path).mkdir(parents=True, exist_ok=True)
-# Path(downloaded_exe_path).mkdir(parents=True, exist_ok=True)
-
-
-# for f in os.listdir(downloads_path):
-#     if f.lower().endswith(('.pdf', '.txt')):
-#         os.rename((downloads_path + '\\' + f), (downloaded_pdf_path + '\\' + f))
-#     elif f.lower().endswith(('.png', '.jpg', '.jpeg')):
-#         os.rename((downloads_path + '\\' + f), (downloaded_images_path + '\\' + f))
-#     elif f.lower().endswith(('.exe')):
-#         os.rename((downloads_path + '\\' + f), (downloaded_exe_path + '\\' + f))
+        new_file_path = downloads_path / FILE_FORMATS[file_format] 
+        new_file_path.mkdir(exist_ok=True)
+        file_path.rename(new_file_path / file)
